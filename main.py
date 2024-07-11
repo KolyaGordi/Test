4479c218f656b4f6f511206d098c62054489b755
KolyaGordi <154602486+KolyaGordi@users.noreply.github.com>
2024-07-11 17:43:34 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import flet as ft
 
-
 def main(page: ft.Page):
-    page.add(ft.SafeArea(ft.Text("Hello, Flet!")))
+    flashlight = ft.Flashlight()
+    page.overlay.append(flashlight)
+    page.add(
+        ft.ElevatedButton(text='asdasdad', on_click=lambda _: flashlight.toggle())
+    )
 
-
-ft.app(main)
+ft.app(target=main, view=ft.AppView.WEB_BROWSER)
